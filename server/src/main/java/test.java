45d603615a051b75c250f2b40f6d96532970c9a6
Tom Bates <tombates6@gmail.com>
2022-03-24 07:46:33 -0600

    add test class

---
+++ server/src/main/java/test.java
@@ -0,0 +1,50 @@
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Base64;
+
+import edu.byu.cs.tweeter.model.domain.AuthToken;
+import edu.byu.cs.tweeter.model.net.request.FollowersCountRequest;
+import edu.byu.cs.tweeter.model.net.request.FollowersRequest;
+import edu.byu.cs.tweeter.model.net.request.LoginRequest;
+import edu.byu.cs.tweeter.model.net.request.RegisterRequest;
+import edu.byu.cs.tweeter.model.net.request.UserRequest;
+import edu.byu.cs.tweeter.model.net.response.FollowersCountResponse;
+import edu.byu.cs.tweeter.model.net.response.FollowersResponse;
+import edu.byu.cs.tweeter.model.net.response.LoginResponse;
+import edu.byu.cs.tweeter.model.net.response.RegisterResponse;
+import edu.byu.cs.tweeter.model.net.response.UserResponse;
+import edu.byu.cs.tweeter.server.dao.S3ImageDAO;
+import edu.byu.cs.tweeter.server.dao.dynamodb.injection.FollowServiceModule;
+import edu.byu.cs.tweeter.server.dao.dynamodb.injection.UserServiceModule;
+import edu.byu.cs.tweeter.server.dao.exceptions.DataAccessException;
+import edu.byu.cs.tweeter.server.service.FollowService;
+import edu.byu.cs.tweeter.server.service.UserService;
+
+public class test {
+//    public static void main(String[] args) {
+//        Injector injector = Guice.createInjector(new UserServiceModule());
+//        UserService service = injector.getInstance(UserService.class);
+//        UserResponse r = service.getUserProfile(new UserRequest("@TomBates", new AuthToken("7a0d3177-174e-4e5a-b3a0-5d337b26b657",1648089456067L)));
+//        int x = 1;
+//    }
+//    public static void main(String[] args) {
+//        Injector injector = Guice.createInjector(new UserServiceModule());
+//        UserService service = injector.getInstance(UserService.class);
+//        LoginResponse r = service.login(new LoginRequest("@TomBates", "tom"));
+//        int x = 1;
+//    }
+//    public static void main(String[] args) {
+//        Injector injector = Guice.createInjector(new FollowServiceModule());
+//        FollowService service = injector.getInstance(FollowService.class);
+//        FollowersCountResponse r = service.getFollowersCount(new FollowersCountRequest(new AuthToken("7a0d3177-174e-4e5a-b3a0-5d337b26b657",1648089456067L), "@TomBates"));
+//        int x = 1;
+//    }
+    public static void main(String[] args) {
+        Injector injector = Guice.createInjector(new FollowServiceModule());
+        FollowService service = injector.getInstance(FollowService.class);
+        FollowersResponse r = service.getFollowers(new FollowersRequest(new AuthToken("7a0d3177-174e-4e5a-b3a0-5d337b26b657",1648089456067L), "@TomBates", 10, null));
+        int x = 1;
+    }
+}
